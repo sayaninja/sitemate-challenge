cd1d6ef0af4d00cc9f843c3d49f10bdcc83eac7f
sayaninja <zhansaya.abdikarimova@gmail.com>
2023-10-08 11:26:18 +0600

    update main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from api.endpoints import issues
+from api.endpoints import issues_router
 from fastapi.responses import ORJSONResponse
 
 app = FastAPI(default_response_class=ORJSONResponse)
 
-app.include_router(issues.router, prefix="/issues", tags=["issues"])
+app.include_router(issues_router.router, prefix="/issues", tags=["issues"])
